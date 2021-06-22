af05e2234427a904bb61a1049af64e9df19b63a4
Frederick Meneses <75036259+avinight@users.noreply.github.com>
2021-06-22 13:22:18 -0400

    Update and rename 9. Palindrome Number to 9. Palindrome Number.py

--- Easy/9. Palindrome Number.py
+++ Easy/9. Palindrome Number.py
@@ -5,3 +5,11 @@ class Solution:
         for ch in str(x):
             rev = ch + rev
         return rev == str(x)
+    
+# Faster Answer
+class Solution:
+def isPalindrome(self, x: int) -> bool:
+    x = str(x)
+    if x == x[::-1]:
+        return True
+    return False
